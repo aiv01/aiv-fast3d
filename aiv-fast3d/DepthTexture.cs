5afc03e0d6bf7b2eaf11ee826929b53d12866717
Roberto De Ioris <roberto@20tab.com>
2017-03-07 12:22:19 +0100

    simplified depth texture

--- aiv-fast3d/DepthTexture.cs
+++ aiv-fast3d/DepthTexture.cs
@@ -9,19 +9,14 @@ namespace Aiv.Fast3D
 		
 		public DepthTexture(int width, int height, int depthSize) : base(width, height)
 		{
-			this.textureId = Graphics.NewTexture();
-			this.Bind();
-			// fix texture type
-			Graphics.TextureDepth(width, height, depthSize);
+			
 			// this leaks a framebuffer object, but unfortunately
 			// it is the best solution for now
 			frameBuffer = Graphics.NewFrameBuffer();
 			Graphics.BindFrameBuffer(frameBuffer);
-
+			// fix texture type
+			Graphics.TextureDepth(width, height, depthSize);
 			Graphics.FrameBufferTextureDepth(this.Id);
-			this.SetNearest();
-			this.SetRepeatX(false);
-			this.SetRepeatY(false);
 
 			Graphics.BindFrameBuffer(Graphics.GetDefaultFrameBuffer());
 		}
