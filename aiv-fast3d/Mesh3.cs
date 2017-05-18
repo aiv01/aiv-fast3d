df8fb09d5ff79d852ec203ef9958746775431733
DuccioLenkowicz <ducciolenkowicz@gmail.com>
2017-05-18 15:56:23 +0200

    Exposed SetRotation via poperty
    Added properties for SetRotation and SetEulerRotation in Mesh3 and
    PerspectiveCamera.
    Preliminarily added QuaternionToPitchYawRoll method to utils.

--- aiv-fast3d/Mesh3.cs
+++ aiv-fast3d/Mesh3.cs
@@ -267,7 +267,23 @@ void main(){
             }
 		}
 
-		private Vector3 position3;
+        public Vector3 Rotation3
+        {
+            set
+            {
+                internalRotation = (Matrix3.CreateRotationX(value.X) * Matrix3.CreateRotationZ(value.Z) * Matrix3.CreateRotationY(value.Y)).ExtractRotation();
+            }
+        }
+
+        public Vector3 EulerRotation3
+        {
+            set
+            {
+                Rotation3 = value * (float)Math.PI / 180f;
+            }
+        }
+
+        private Vector3 position3;
 		public Vector3 Position3
 		{
 			get
--- aiv-fast3d/Utils.cs
+++ aiv-fast3d/Utils.cs
@@ -33,5 +33,39 @@ namespace Aiv.Fast3D
 		{
 			return LookAt(direction, Vector3.UnitY);
 		}
-	}
+
+
+        // from http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/index.htm
+        public static Vector3 QuaternionToPitchYawRoll(Quaternion q)
+        {
+            float yaw, pitch, roll;
+            float test = q.X * q.Y + q.Z * q.W;
+
+            // 90 deg roll singularity
+            if (test > 0.499f)
+            {
+                yaw   = 2f * (float)Math.Atan2(q.X, q.W);
+                roll  = MathHelper.PiOver2;
+                pitch = 0f;
+            }
+            // -90 deg roll singularity
+            if (test < -0.499f)
+            {
+                yaw   = -2f * (float)Math.Atan2(q.X, q.W);
+                roll  = -MathHelper.PiOver2;
+                pitch = 0f;
+            }
+            else
+            {
+                float sqx = q.X * q.X;
+                float sqy = q.Y * q.Y;
+                float sqz = q.Z * q.Z;
+                yaw   = (float)Math.Atan2(2 * q.Y * q.W - 2 * q.X * q.Z, 1 - 2 * sqy - 2 * sqz);
+                roll  = (float)Math.Asin(2 * test);
+                pitch = (float)Math.Atan2(2 * q.X * q.W - 2 * q.Y * q.Z, 1 - 2 * sqx - 2 * sqz);
+            }
+
+            return new Vector3(pitch, yaw, roll);
+        }
+    }
 }
