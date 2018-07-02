2dafa1e313a122d14cb5c0c2335b8f4163e16d5b
Roberto De Ioris <roberto@20tab.com>
2018-07-02 18:32:00 +0200

    0.10.0

--- aiv-fast3d/DepthTexture.cs
+++ aiv-fast3d/DepthTexture.cs
@@ -4,16 +4,24 @@ using OpenTK.Graphics.OpenGL;
 
 namespace Aiv.Fast3D
 {
-	public class DepthTexture : RenderTexture
-	{
+    public class DepthTexture : RenderTexture
+    {
 
-		public DepthTexture(int width, int height, int depthSize, bool clampToBorder = true) : base(width, height, false, depthSize, true)
-		{
-			if (clampToBorder)
-			{
-				Graphics.TextureClampToBorderX(1f, 1f, 1f, 1f);
-				Graphics.TextureClampToBorderY(1f, 1f, 1f, 1f);
-			}
-		}
-	}
+        public virtual float[] DownloadFloat(int mipMap = 0)
+        {
+            float[] data = new float[this.Width * this.Height * sizeof(float)];
+            this.Bind();
+            Graphics.DepthTextureGetPixels(-1, mipMap, data);
+            return data;
+        }
+
+        public DepthTexture(int width, int height, int depthSize, bool clampToBorder = true) : base(width, height, false, depthSize, true)
+        {
+            if (clampToBorder)
+            {
+                Graphics.TextureClampToBorderX(1f, 1f, 1f, 1f);
+                Graphics.TextureClampToBorderY(1f, 1f, 1f, 1f);
+            }
+        }
+    }
 }
